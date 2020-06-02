6af3a53d48bb57935df4c8228579471329766766
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 20:00:29 +0000

    Automatically backed up by Learn

---
+++ my_each.rb
@@ -0,0 +1,19 @@
+module EachBetter
+  def my_each
+    if block_given?
+      i = 0 
+      while i < self.size
+        yield(self[i])
+        i += 1
+      end
+    end
+  end
+end
+
+class Array
+  include EachBetter
+end
+
+[2,10,12,15].my_each                    
+[2,10,12,15].my_each {puts 'Raymond'}   
+[2,10,12,15].my_each {|e| puts e * e}
