db3929a6bcba53bc2393dfba7a00a95294427820
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 19:46:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -11,6 +11,6 @@
 end
 
 
-[2,10,12,15].my_each                    # no block
-[2,10,12,15].my_each {puts 'Raymond'}   # block and no parameter
+[2,10,12,15].my_each                    
+[2,10,12,15].my_each {puts 'Raymond'}   
 [2,10,12,15].my_each {|e| puts e * e}
