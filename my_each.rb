d8d72c3f5708de06e1bad48b083765829c176ba2
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 19:46:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+  def my_each
+    if block_given?
+      i = 0 
+      while i < self.size
+        yield(self[i])
+        i += 1
+      end
+    end
+  end
+  
 end
+
+
+[2,10,12,15].my_each                    # no block
+[2,10,12,15].my_each {puts 'Raymond'}   # block and no parameter
+[2,10,12,15].my_each {|e| puts e * e}
